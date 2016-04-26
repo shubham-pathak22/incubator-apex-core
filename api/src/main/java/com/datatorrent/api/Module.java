tushargosavi/apex-core

APEXCORE-107 Support for adding modules in the DAG property file and json file. Change module API to make module as an operator.

api/src/main/java/com/datatorrent/api/Module.java
@@ -36,7 +36,7 @@ import com.datatorrent.api.Operator.Unifier;
  * @since 3.3.0
  */
 @InterfaceStability.Evolving
-public interface Module
+public interface Module extends Operator
 {
   void populateDAG(DAG dag, Configuration conf);
 

engine/src/main/java/com/datatorrent/stram/codec/LogicalPlanSerializer.java
@@ -362,7 +362,7 @@ public class LogicalPlanSerializer extends JsonSerializer<LogicalPlan>
     Map<String, Object> moduleDetailMap = new HashMap<>();
     ArrayList<String> operatorArray = new ArrayList<>();
     moduleDetailMap.put("name", moduleMeta.getName());
-    moduleDetailMap.put("className", moduleMeta.getModule().getClass().getName());
+    moduleDetailMap.put("className", moduleMeta.getOperator().getClass().getName());
 
     moduleDetailMap.put("operators", operatorArray);
     for (OperatorMeta operatorMeta : moduleMeta.getDag().getAllOperators()) {

engine/src/main/java/com/datatorrent/stram/webapp/StramWebServices.java
@@ -788,7 +788,7 @@ public class StramWebServices
       if (logicalModule == null) {
         throw new NotFoundException();
       }
-      operatorProperties = LogicalPlanConfiguration.getObjectProperties(logicalModule.getModule());
+      operatorProperties = LogicalPlanConfiguration.getObjectProperties(logicalModule.getOperator());
     } else {
       operatorProperties = LogicalPlanConfiguration.getObjectProperties(logicalOperator.getOperator());
     }
@@ -850,7 +850,7 @@ public class StramWebServices
   {
     init();
     return new JSONObject(objectMapper.writeValueAsString(LogicalPlanSerializer.convertToMap(
-        dagManager.getLogicalPlan(), includeModules != null)));
+        dagManager.getLogicalPlan(), includeModules != null && includeModules.equals("[true]"))));
   }
 
   @POST // not supported by WebAppProxyServlet, can only be called directly

shubham-pathak22/incubator-apex-core

api/src/main/java/com/datatorrent/api/Module.java
@@ -36,7 +36,7 @@ import com.datatorrent.api.Operator.Unifier;
  * @since 3.3.0
  */
 @InterfaceStability.Evolving
-public interface Module
+public interface Module extends DagNode
 {
   void populateDAG(DAG dag, Configuration conf);
 

engine/src/main/java/com/datatorrent/stram/codec/LogicalPlanSerializer.java
@@ -362,7 +362,7 @@ public class LogicalPlanSerializer extends JsonSerializer<LogicalPlan>
     Map<String, Object> moduleDetailMap = new HashMap<>();
     ArrayList<String> operatorArray = new ArrayList<>();
     moduleDetailMap.put("name", moduleMeta.getName());
-    moduleDetailMap.put("className", moduleMeta.getModule().getClass().getName());
+    moduleDetailMap.put("className", moduleMeta.getNode().getClass().getName());
 
     moduleDetailMap.put("operators", operatorArray);
     for (OperatorMeta operatorMeta : moduleMeta.getDag().getAllOperators()) {

engine/src/main/java/com/datatorrent/stram/webapp/StramWebServices.java
@@ -788,7 +788,7 @@ public class StramWebServices
       if (logicalModule == null) {
         throw new NotFoundException();
       }
-      operatorProperties = LogicalPlanConfiguration.getObjectProperties(logicalModule.getModule());
+      operatorProperties = LogicalPlanConfiguration.getObjectProperties(logicalModule.getNode());
     } else {
       operatorProperties = LogicalPlanConfiguration.getObjectProperties(logicalOperator.getOperator());
     }
@@ -850,7 +850,7 @@ public class StramWebServices
   {
     init();
     return new JSONObject(objectMapper.writeValueAsString(LogicalPlanSerializer.convertToMap(
-        dagManager.getLogicalPlan(), includeModules != null)));
+        dagManager.getLogicalPlan(), includeModules != null && includeModules.equals("[true]"))));
   }
 
   @POST // not supported by WebAppProxyServlet, can only be called directly